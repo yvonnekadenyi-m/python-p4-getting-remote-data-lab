2bda2023820fe1876ff482b91439b851922482f0
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 20:13:59 +0300

    added to app.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,7 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
